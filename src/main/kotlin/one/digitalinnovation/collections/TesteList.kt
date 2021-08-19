bfd2bd92157543e246c5f6aaa3f51d5d660a5c83
antonysf <antony.figueiredo@gmail.com>
2021-08-19 19:12:32 -0300

    organizando a lista com sortedBy organizando grupos com groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,10 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao = Funcionario("joao", 1000.0 )
-    val pedro = Funcionario("pedro", 2000.0 )
-    val maria = Funcionario("maria", 4000.0 )
+    val joao = Funcionario("joao", 2000.0, "CLT")
+    val pedro = Funcionario("pedro", 1500.0, "PJ")
+    val maria = Funcionario("maria", 4000.0, "CLT" )
+
 
     val funcionarios =  listOf(joao, pedro, maria)
 
@@ -12,11 +13,18 @@ fun main(){
     println("-----------------------------------------")
     println(funcionarios.find { it.nome == "maria" })
 
+    println("-----------------------------------------")
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+    println("-----------------------------------------")
+    funcionarios.groupBy { it.tipoContatacao }.forEach { println(it) }
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContatacao: String
 ){
     override fun toString(): String =
         """
